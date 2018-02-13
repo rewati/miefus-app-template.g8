c6ee04dc737398695048d984270700fda6b6dca4
rewatiraman <rewati.raman@ytel.com>
2018-02-12 16:15:13 -0800

    Minor changes to Application

--- src/main/g8/src/main/scala/com/ytel/myapp/Application.scala
+++ src/main/g8/src/main/scala/com/ytel/myapp/Application.scala
@@ -1,24 +1,17 @@
 package com.ytel.minions.myapp
 
-import akka.actor.{ActorRef, Props}
-import akka.routing.BalancingPool
-import com.ytel.miefus._
+import com.ytel.miefus.{KafkaStreamProcessor, _}
 
 object Application extends MiefusApplication with KafkaStreamProcessor  {
   override val streamName = "MyStream"
-  lazy val processor: ActorRef =
-    ClusterCommon.system.actorOf(BalancingPool(100).props(Props[MyMessageActor]), "MyMessageActor")
-  override val processingActors = List(processor)
+  override val processingActors = List(processor[MyMessageProcessor]("MyMessageProcessor"))
   kafkaStream.initialize
 }
 
-class MyMessageActor extends CommonActor {
-  lazy val errorTopic = Configuration("error_topics","myapp_error_topic")
-  override def receive = {
-    case x: KafkaMessage =>
-      log.info(s"Message Received by MyMessageActor: "+x.msg)
-    case x =>
-      log.error(s"Dont know what to do with message: "+x)
+class MyMessageProcessor extends MessageProcessor  {
+  override def messageHandler(m: KafkaMessage): Unit = {
+    // Handle message
+    log.debug(s"New message received: "+m.msg)
   }
 }
 
