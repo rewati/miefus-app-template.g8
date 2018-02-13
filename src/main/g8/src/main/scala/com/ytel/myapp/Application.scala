754a8456637ff9f7f5cf988f051ad1a767ee4aab
rewatiraman <rewati.raman@ytel.com>
2018-02-12 16:21:30 -0800

    Minor changes to Application

--- src/main/g8/src/main/scala/com/ytel/myapp/Application.scala
+++ src/main/g8/src/main/scala/com/ytel/myapp/Application.scala
@@ -2,16 +2,19 @@ package com.ytel.minions.myapp
 
 import com.ytel.miefus.{KafkaStreamProcessor, _}
 
-object Application extends MiefusApplication with KafkaStreamProcessor  {
-  override val streamName = "MyStream"
-  override val processingActors = List(processor[MyMessageProcessor]("MyMessageProcessor"))
-  kafkaStream.initialize
-}
-
+/**
+  * Start writing your message handler..
+  */
 class MyMessageProcessor extends MessageProcessor  {
   override def messageHandler(m: KafkaMessage): Unit = {
     // Handle message
     log.debug(s"New message received: "+m.msg)
   }
+}
+
+object Application extends MiefusApplication with KafkaStreamProcessor  {
+  override val streamName = "MyStream"
+  override val processingActors = List(processor[MyMessageProcessor]("MyMessageProcessor"))
+  kafkaStream.initialize
 }
 
